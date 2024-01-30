3b6b2917c7d84d158aba2cc584b030cecb1833b3
Hrihorii Ilin <devopsick@pm.me>
2024-01-30 02:50:29 +0000

    vercel add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [],
+  integrations: [tailwind(), react(), sitemap()],
   redirects: {
     '/': '/en/',
   },
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
